289d266d0f76e92ac5025781bb2580e823980d9e
mikelnord <mikelnord@gmail.com>
2020-10-15 08:56:13 +1000

    commit 2

--- src/Practice1.java
+++ src/Practice1.java
@@ -6,14 +6,41 @@ public class Practice1 {
         long l = 1000000L;
         float f = 3.14f;
         double d = 3.14;
-        char c='c';
-        boolean bool=true;
-        System.out.println(vich(5,10,15,3.14));
+        char c = 'c';
+        boolean bool = true;
+
     }
-    public static double vich(int a, int b, int c, double d){
-        if(d!=0){
+
+    public static double vich(int a, int b, int c, double d) {
+        if (d != 0) {
             return a * (b + (c / d));
         }
         return 0;
+    }
+
+    public static boolean checkSum(int a, int b) {
+        int d = a + b;
+        return d > 10 && d <= 20;
+    }
+
+    public static void printInt(int a) {
+        if (a < 0) System.out.println("Число отрицательное");
+        else System.out.println("Число положительное");
+    }
+
+    public static boolean isNegativ(int a) {
+        return a < 0;
+    }
+
+    public static void privet(String name) {
+        System.out.println("Привет, " + name + "!");
+    }
+
+    public static void isVis(int year) {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.println(year + " - високосный");
+        } else {
+            System.out.println(year + " - не високосный");
+        }
     }
 }
