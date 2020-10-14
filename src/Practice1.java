c8b0abc9ba0a48c2a8b9f5877c86bdf7c98f93e1
mikelnord <mikelnord@gmail.com>
2020-10-14 17:32:36 +1000

    commit 2

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,5 +1,19 @@
 public class Practice1 {
     public static void main(String[] args) {
-
+        byte b = 120;
+        short sh = 3000;
+        int i = 100;
+        long l = 1000000L;
+        float f = 3.14f;
+        double d = 3.14;
+        char c='c';
+        boolean bool=true;
+        System.out.println(vich(5,10,15,3.14));
+    }
+    public static double vich(int a, int b, int c, double d){
+        if(d!=0){
+            return a * (b + (c / d));
+        }
+        return 0;
     }
 }
